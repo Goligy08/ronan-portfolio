0297f97578bf8324e14c5886222e15ce34d51d4a
Goligy08 <r.goligy@hotmail.co.uk>
2023-10-04 12:54:38 +0100

    Updates

---
+++ precache-manifest.be2eb8fe8ba127340d1707576e2f0e10.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f8fd72cfddc1ee058c941b00d9b48743",
+    "url": "/ronan-portfolio/index.html"
+  },
+  {
+    "revision": "1275dc0703cfbdccfe9e",
+    "url": "/ronan-portfolio/static/css/main.e5884091.chunk.css"
+  },
+  {
+    "revision": "d76c37ecf342a89d62ba",
+    "url": "/ronan-portfolio/static/js/2.91bba417.chunk.js"
+  },
+  {
+    "revision": "d4058960d4b974f23a50",
+    "url": "/ronan-portfolio/static/js/3.ecac36d1.chunk.js"
+  },
+  {
+    "revision": "1275dc0703cfbdccfe9e",
+    "url": "/ronan-portfolio/static/js/main.8314b367.chunk.js"
+  },
+  {
+    "revision": "edf4810ccdda2fe7a40c",
+    "url": "/ronan-portfolio/static/js/runtime~main.71cfb60a.js"
+  },
+  {
+    "revision": "67632eb458812abc3a29e7cabcc0c7d0",
+    "url": "/ronan-portfolio/static/media/Hero7.67632eb4.svg"
+  },
+  {
+    "revision": "7f5bd00f91ead40d40bb58a19d8146d5",
+    "url": "/ronan-portfolio/static/media/Hero8.7f5bd00f.svg"
+  },
+  {
+    "revision": "5dbec98f6616ad85f86a9272c46df624",
+    "url": "/ronan-portfolio/static/media/Portfolio Logo DarkMode.5dbec98f.svg"
+  },
+  {
+    "revision": "4b7e41a50524aac88a1bff5c5a6624d3",
+    "url": "/ronan-portfolio/static/media/Portfolio Logo LightMode.4b7e41a5.svg"
+  },
+  {
+    "revision": "e3327a5c66ca9b18c10f35fef847a4ae",
+    "url": "/ronan-portfolio/static/media/mobileHero.e3327a5c.svg"
+  },
+  {
+    "revision": "1a9abce1ae03fc7bfd231536b8e25986",
+    "url": "/ronan-portfolio/static/media/mobileHeroDark.1a9abce1.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.47430dafaaa86af2a58b04eb01cb307e.js"
+  "/ronan-portfolio/precache-manifest.be2eb8fe8ba127340d1707576e2f0e10.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/ronan-portfolio/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
